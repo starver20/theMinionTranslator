bdb5fd1b70f0fdb9cd6e5661d5ddc880411f2d80
amarnarute <amarnarute2002@gmail.com>
2021-05-01 17:11:51 +0530

    responsive design

---
+++ app.js
@@ -0,0 +1,19 @@
+var btnInput = document.querySelector('#btn-input');
+var input = document.querySelector('#txt-input');
+var output = document.querySelector('#txt-output');
+
+// console.log(text);
+
+btnInput.addEventListener('click', () => {
+  translate(input.value);
+});
+
+const URLgenerator = (text) => {
+  return `https://api.funtranslations.com/translate/minion.json?text=${text}`;
+};
+
+const translate = (text) => {
+  fetch(URLgenerator(text))
+    .then((res) => res.json())
+    .then((resJSON) => (output.innerText = resJSON.contents.translated));
+};
